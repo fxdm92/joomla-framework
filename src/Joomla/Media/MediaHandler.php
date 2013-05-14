9bf90e11ca9f20452c1dd190191260abf3ab0a99
Pein <a0075105@nus.edu.sg>
2013-05-15 00:50:33 +0700

    Add new Media folder in Joomla folder.

---
+++ src/Joomla/Media/MediaHandler.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Part of the Joomla Framework Logger Package
+ *
+ * @copyright  Copyright (C) 2005 - 2013 Open Source Matters, Inc. All rights reserved.
+ * @license    GNU General Public License version 2 or later; see LICENSE
+ */
+
+namespace Joomla\Media;
+
+/**
+ * Joomla! Media Handler class
+ *
+ * This class is the api of media handlers.
+ * @since  3.0
+ */
+
+class MediaHandler
+{
+
+}
